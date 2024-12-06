6e422c0539138a3fdb43b83396329f89630e3023
I'm Vu <thienvu.dev@gmail.com>
2024-12-07 01:25:21 +0700

    FIXXX

--- src/shared/axios.ts
+++ src/shared/axios.ts
@@ -4,7 +4,7 @@ import { API_URL } from "./constants";
 const instance = axios.create({
   baseURL: API_URL,
   params: {
-    api_key: process.env.TMDB_KEY,
+    api_key: process.env.REACT_APP_API_KEY,
   },
 });
 
